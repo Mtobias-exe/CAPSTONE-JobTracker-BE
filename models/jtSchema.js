c208283040dc717f7c7296bf7c7a6cad6b11b22b
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-28 11:09:21 -0500

    added jobdata model schema

---
+++ models/jtSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const jobDataModel = new mongoose.Schema({
+    companyName:{
+        type: String,
+        required: true
+    },
+    
+    jobTitle: {
+        type: String,
+        required: true
+    },
+    
+    date: {
+        type: Date,
+        required: true
+    },
+    
+    status:{
+        type: String,
+        enum: ['pending', 'rejected', 'accepted'],
+        required: true
+    },
+    
+    notes:{
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model("JobData", jobDataModel);
